61aa5589b159cc2aba17cb8ea79844f71839d1dc
Negative <negggative@gmail.com>
2023-02-26 15:07:37 -0400

    Add listener for context commands

--- src/main/java/games/negative/framework/discord/command/listener/SlashCommandListener.java
+++ src/main/java/games/negative/framework/discord/command/listener/SlashCommandListener.java
@@ -1,10 +1,13 @@
 package games.negative.framework.discord.command.listener;
 
+import games.negative.framework.discord.command.ContextCommand;
 import games.negative.framework.discord.command.SlashCommand;
 import games.negative.framework.discord.command.map.CommandMap;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
@@ -38,5 +41,45 @@ public class SlashCommandListener extends ListenerAdapter {
         globalCommands.stream()
                 .filter(slashCommand -> slashCommand.getName().equalsIgnoreCase(event.getName()))
                 .findFirst().ifPresent(slashCommand -> slashCommand.runCommand(event));
+    }
+
+    @Override
+    public void onMessageContextInteraction(@NotNull MessageContextInteractionEvent event) {
+        Guild guild = event.getGuild();
+        if (guild != null) {
+            Collection<ContextCommand> commands = commandMap.getServerContextCommands(guild.getId());
+            ContextCommand command = commands.stream().filter(cmd -> cmd.getName().equalsIgnoreCase(event.getName())).findFirst().orElse(null);
+            if (command != null) {
+                command.onInteractMessage(event);
+                return;
+            }
+        }
+
+        Collection<ContextCommand> commands = commandMap.getGlobalContextCommands();
+        ContextCommand command = commands.stream().filter(cmd -> cmd.getName().equalsIgnoreCase(event.getName())).findFirst().orElse(null);
+        if (command == null)
+            return;
+
+        command.onInteractMessage(event);
+    }
+
+    @Override
+    public void onUserContextInteraction(@NotNull UserContextInteractionEvent event) {
+        Guild guild = event.getGuild();
+        if (guild != null) {
+            Collection<ContextCommand> commands = commandMap.getServerContextCommands(guild.getId());
+            ContextCommand command = commands.stream().filter(cmd -> cmd.getName().equalsIgnoreCase(event.getName())).findFirst().orElse(null);
+            if (command != null) {
+                command.onInteractUser(event);
+                return;
+            }
+        }
+
+        Collection<ContextCommand> commands = commandMap.getGlobalContextCommands();
+        ContextCommand command = commands.stream().filter(cmd -> cmd.getName().equalsIgnoreCase(event.getName())).findFirst().orElse(null);
+        if (command == null)
+            return;
+
+        command.onInteractUser(event);
     }
 }
